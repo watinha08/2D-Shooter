b63d53a565ca6da1dc2d04e4cbf61a9fea7b7d10
Nicolas Tetsuo Kimura <nicolas.t.kimura@gmail.com>
2023-08-14 21:07:26 -0300

    Enemy and Enemy Spawner
    Enemy Movement and collision.
    Player takes damage on collision with enemys.
    EnemySpawner.

--- 20minTillDawn/Assets/Scripts/Player.cs
+++ 20minTillDawn/Assets/Scripts/Player.cs
@@ -5,13 +5,24 @@ using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float velocity;
+    public static Player instance;
+
+    [SerializeField] private float velocity = 10;
+    [SerializeField] private int lives = 3;
 
     private Animator animator;
-    private Transform playerTransform;
+    public Transform playerTransform;
 
     private void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
         animator = GetComponent<Animator>();
         playerTransform = GetComponent<Transform>();
     }
@@ -23,9 +34,24 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
-        float moveX = Input.GetAxis("Horizontal") * velocity * Time.deltaTime;
-        float moveY = Input.GetAxis("Vertical") * velocity * Time.deltaTime;
+        float moveX = Input.GetAxisRaw("Horizontal") * velocity * Time.deltaTime;
+        float moveY = Input.GetAxisRaw("Vertical") * velocity * Time.deltaTime;
 
         playerTransform.Translate(new Vector3 (moveX, moveY, 0));
+
+        if (lives <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public Vector3 GetPlayerPosition()
+    {
+        return playerTransform.position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        lives--;
     }
 }
